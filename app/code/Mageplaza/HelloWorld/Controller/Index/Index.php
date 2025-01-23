a6028fc5e0993b874eed1049df46ae226c84815f
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-23 20:06:12 +0530

    Creating Index.php

---
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Mageplaza\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $result = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        return $result;
+    }
+}
