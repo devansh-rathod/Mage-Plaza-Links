95c34c407d04eb5610067e1d3b481fdb20d3601e
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-25 21:34:22 +0530

    Updating index.php to instantiate the model object

--- app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -1,9 +1,33 @@
 <?php
-namespace Mageplaza\HelloWorld\Block;
+namespace Mageplaza\HelloWorld\Controller\Index;
 
-use Magento\Framework\View\Element\Template;
-
-class Index extends Template
+class Index extends \Magento\Framework\App\Action\Action
 {
-    // Custom methods can be added here
+	protected $_pageFactory;
+
+	protected $_postFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Mageplaza\HelloWorld\Model\PostFactory $postFactory
+		)
+	{
+		$this->_pageFactory = $pageFactory;
+		$this->_postFactory = $postFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		$post = $this->_postFactory->create();
+		$collection = $post->getCollection();
+		foreach($collection as $item){
+			echo "<pre>";
+			print_r($item->getData());
+			echo "</pre>";
+		}
+		exit();
+		return $this->_pageFactory->create();
+	}
 }
